a78742aad2f611700db859297cd97949f05013be
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2017-06-20 08:35:01 -0300

    Fixing the problem with Zip: when the array a is less than array b

--- util/util.go
+++ util/util.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Tuple struct {
-	a, b float64
+	A, B float64
 }
 
 func Zip(a, b []float64) ([]Tuple, error) {
@@ -14,11 +14,19 @@ func Zip(a, b []float64) ([]Tuple, error) {
 		return nil, fmt.Errorf("Zip: one of the arguments is empty")
 	}
 
-	r := make([]Tuple, len(a), len(a))
+	if len(a) < len(b) {
+		return createTuples(a, b), nil
+	}
+	return createTuples(a[:len(b)], b), nil
+
+}
+
+func createTuples(a, b []float64) []Tuple {
+	tuples := make([]Tuple, len(a), len(a))
 
 	for i, e := range a {
-		r[i] = Tuple{e, b[i]}
+		tuples[i] = Tuple{e, b[i]}
 	}
+	return tuples
 
-	return r, nil
 }
--- util/util_test.go
+++ util/util_test.go
@@ -3,30 +3,31 @@ package util
 import (
 	"reflect"
 	"testing"
-
-	"github.com/rpinheiroalmeida/linalg/vector"
 )
 
 func TestZip(t *testing.T) {
 	cases := []struct {
-		v1   vector.Vector
-		v2   vector.Vector
+		v1   []float64
+		v2   []float64
 		want []Tuple
 	}{
 		{
-			vector.Vector{1.0, 2.0, 3.0}, vector.Vector{1.0, 3.0, 5.0},
-			[]Tuple{Tuple{a: 1.0, b: 1.0}, Tuple{a: 2.0, b: 3.0}, Tuple{a: 3.0, b: 5.0}},
+			[]float64{1.0, 2.0, 3.0}, []float64{1.0, 3.0, 5.0},
+			[]Tuple{Tuple{A: 1.0, B: 1.0}, Tuple{A: 2.0, B: 3.0}, Tuple{A: 3.0, B: 5.0}},
 		},
 		{
-			vector.Vector{1.0, 2.0}, vector.Vector{1.0, 3.0, 5.0},
-			[]Tuple{Tuple{a: 1.0, b: 1.0}, Tuple{a: 2.0, b: 3.0}},
+			[]float64{1.0, 2.0}, []float64{1.0, 3.0, 5.0},
+			[]Tuple{Tuple{A: 1.0, B: 1.0}, Tuple{A: 2.0, B: 3.0}},
 		},
 		{
-			vector.Vector{1.0}, vector.Vector{1.0, 3.0, 5.0},
-			[]Tuple{Tuple{a: 1.0, b: 1.0}},
+			[]float64{1.0}, []float64{1.0, 3.0, 5.0},
+			[]Tuple{Tuple{A: 1.0, B: 1.0}},
+		},
+		{
+			[]float64{1.0, 2.0}, []float64{1.0},
+			[]Tuple{Tuple{A: 1.0, B: 1.0}},
 		},
 	}
-
 	for _, c := range cases {
 		gotZip, _ := Zip(c.v1, c.v2)
 		if !reflect.DeepEqual(gotZip, c.want) {
@@ -37,11 +38,11 @@ func TestZip(t *testing.T) {
 
 func TestZip_WhenOneOfParametersIsEmpty(t *testing.T) {
 	cases := []struct {
-		v1 vector.Vector
-		v2 vector.Vector
+		v1 []float64
+		v2 []float64
 	}{
-		{vector.Vector{}, vector.Vector{1.0, 3.0, 5.0}},
-		{vector.Vector{1.0, 2.0}, vector.Vector{}},
+		{[]float64{}, []float64{1.0, 3.0, 5.0}},
+		{[]float64{1.0, 2.0}, []float64{}},
 	}
 
 	for _, c := range cases {
